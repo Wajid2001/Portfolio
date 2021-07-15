c1ecee11e1daa6c850aaa38413be0f1f6b08219f
Wajid2001 <s.w.a.11Wajid@gmail.com>
2021-07-15 13:05:26 +0530

    Update

--- React_Files/App.jsx
+++ React_Files/App.jsx
@@ -1,64 +1,28 @@
 import React, { Component, lazy, Suspense } from "react";
-import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
 
 import ReactDOM from "react-dom";
+import Card from "./Components/Card";
 
-const Header = lazy(() => import("./Components/Header"));
-const Home = lazy(() => import("./Pages/Home"));
-const Sigin = lazy(() => import("./Pages/Signin"));
-
-require("./static/layout.scss");
-
-const csrfToken = () => {
-	const token = document.querySelector("input[name='csrfmiddlewaretoken']").value;
-	console.log(`csrf token : ${token}`);
-	document.querySelector("input[name='csrfmiddlewaretoken']").remove();
-	return token;
-};
-
-const csrfCookie = () => {
-	let name = "csrftoken";
-	let cookieValue = null;
-	if (document.cookie && document.cookie !== "") {
-		const cookies = document.cookie.split(";");
-		for (let i = 0; i < cookies.length; i++) {
-			const cookie = cookies[i].trim();
-			// Does this cookie string begin with the name we want?
-			if (cookie.substring(0, name.length + 1) === name + "=") {
-				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-				break;
-			}
-		}
-	}
-	console.log(`csrfCookie : ${cookieValue}`);
-	return cookieValue;
-};
-
-csrfToken();
-csrfCookie();
+document.querySelector("body").className = "";
 
 class App extends Component {
 	render() {
 		return (
 			<>
-				<div className='cursive text-6xl text-center font-extrabold shadow-sm'>
-					Wajid Areeb
+				<div className='h-screen flex flex-col shadow-md z-40'>
+					<div className='w-full select-none my-auto pb-4  cursive text-6xl text-center font-extrabold sticky top-0'>
+						Wajid Areeb
+					</div>
+					<a className='ml-auto' href='https://github.com/Wajid2001'>
+						Github
+					</a>
 				</div>
-				<h1 className='text-4xl font-bold'>
-					Project 1:
-					<br />
-					Ecommerce
-				</h1>
-				This is <div className='code'>working</div>.
-				<BrowserRouter>
-					<Suspense fallback={<div className='loading'></div>}>
-						<Switch>
-							<Route exact path='/' component={Home} />
-							<Route exact path='/header' component={Header} />
-							<Route exact path='/signin' component={Sigin} />
-						</Switch>
-					</Suspense>
-				</BrowserRouter>
+
+				<Suspense fallback={<div className='loading'></div>}>
+					<div className='z-0 bg-gray-50 h-screen flex border-t'>
+						<Card />
+					</div>
+				</Suspense>
 			</>
 		);
 	}
---
+++ React_Files/Components/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Card() {
+	return (
+		<div className='mx-2 my-16 px-3 py-2  space-y-2 rounded-lg'>
+			<h1 className='text-4xl'>Project 1:</h1>
+			<div className='border-2 border-gray-900 py-1 px-2 flex'>
+				<h2 className='text-3xl'>Commerce</h2>
+			</div>
+			<div className=''>This website made very much latest technologies</div>
+		</div>
+	);
+}
